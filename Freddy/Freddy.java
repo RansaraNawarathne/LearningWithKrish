8b8aef94b5c2a2eeb1cafba1b162fb1749bbdba5
Malindu Nawarathne <ransaranawarathne@gmail.com>
2022-01-02 15:57:25 +0530

    New version of Freddy Added.

--- Freddy/Freddy.java
+++ Freddy/Freddy.java
@@ -2,20 +2,26 @@ import java.util.Scanner;
 
 class Freddy {
 	public static void main (String[] args){
+		//Declaring and initializing jump distance, rest time array and  distance variable.
 		int[] jumpDistance = {5,3,1};
 		double[] restTime = {1,2,3.5};
 		int distance = 0;
+
+		//Promting question to the user and assigning user input value to the distance variable.
 		System.out.println ("Enter distance that Freddy need to move?");
 		Scanner sc = new Scanner (System.in);
 		distance = sc.nextInt ();
 		sc.close();
+
+		//Declaring and initializing array indexes and related variables.
 		int jumpDistanceIndex = 0;
 		int hopCount = 0;
 		int restTimeIndex = 0;
 		double timeTaken = 0.0;
+
+		//Calculating hop count according to the distance.
 		while (distance > 0) {
-			distance = distance - jumpDistance[jumpDistanceIndex];
-			//timeTaken = timeTaken + restTime[jumpDistanceIndex];
+			distance -= jumpDistance[jumpDistanceIndex];
 			hopCount++;
 			if (jumpDistanceIndex == 2) {
 				jumpDistanceIndex = 0;
@@ -23,17 +29,23 @@ class Freddy {
 				jumpDistanceIndex++;
 			}
 		}
+
+		//Calculating total rest time according to the hop count.
 		int tempCount = 0;
 		for (int count = 1; count < hopCount; count++){		
 			if(tempCount >= restTime.length){
 				tempCount = 0;
-				timeTaken = timeTaken + restTime[tempCount];
-			} else {
-				timeTaken = timeTaken + restTime[tempCount];
 			}
+			timeTaken += restTime[tempCount];
 			tempCount++;		
 		}
+		
+		//Displaying calculated values.
 		System.out.println ("Hop(s) Count : " + hopCount);
-		System.out.println ("Time Taken : " + timeTaken + " Seconds");
+		if (timeTaken  == 1) {
+			System.out.println ("Total Rest Time : " + timeTaken  + " second");
+		} else {
+			System.out.println ("Total Rest Time : " + timeTaken  + " seconds");
+		}
 	}
 }
