f7a29fda31b07fd0b159319011a94de5d3d0e95d
Malindu Nawarathne <ransaranawarathne@gmail.com>
2022-01-13 09:21:03 +0530

    GIT Initial Commit

---
+++ src/main/java/com/malindu/rentacar/demoapplication/controller/MainController.java
@@ -0,0 +1,31 @@
+package com.malindu.rentacar.demoapplication.controller;
+
+import com.malindu.rentacar.demoapplication.model.Student;
+import com.malindu.rentacar.demoapplication.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @Autowired
+    StudentService studentService;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String greeting () {
+        return "Hello Springboot";
+    }
+
+    @RequestMapping(value = "/hello", method = RequestMethod.POST)
+    public String greeting2 () {
+        return "Hello Springboot from POST method";
+    }
+
+    @RequestMapping(value = "/student", method = RequestMethod.POST)
+    public Student save (@RequestBody Student student) {
+        return studentService.save(student);
+    }
+}
